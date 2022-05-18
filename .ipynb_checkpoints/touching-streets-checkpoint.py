c08119d4dd075d7c1ca55a15781dc8b70ba36b45
jamaps <jeffallen89@gmail.com>
2022-05-18 10:14:02 -0400

    find connecting streets for intersection geocoding

---
+++ .ipynb_checkpoints/touching-streets-checkpoint.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+
+dfs = gpd.read_file('data/centreline/Centreline - Version 2.geojson')
+
+dfs["connecting_streets"] = None  
+
+i = 0
+
+for index, street in dfs.iterrows():
+
+    # get touching streets
+    constr = dfs[dfs.geometry.touches(street.geometry)].LINEAR_NAME_FULL.tolist()
+
+    # remove own name of the country from the list
+    constr = [ name for name in constr if street.LINEAR_NAME_FULL != name ]
+    
+    # remove duplicates
+    constr = list(set(constr))
+
+    # add names of neighbors as NEIGHBORS value
+    dfs.at[index, "connecting_streets"] = ", ".join(constr)
+    
+    i+=1
+    
+    if i % 100 == 0:
+        print(i)
+        
+# save as a new file
+dfs.to_file('data/centreline/centreline-with-connections.geojson')
+    
