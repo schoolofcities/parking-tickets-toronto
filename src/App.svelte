74ec1c8e87ac603ab147d873f47b725e71a47608
jamaps <jeffallen89@gmail.com>
2022-06-02 13:25:21 -0400

    svelte

---
+++ src/App.svelte
@@ -0,0 +1,66 @@
+<script>
+	import logo from './assets/logo.png'
+	import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+	<div id="bar"></div>
+	<img src={logo} alt="School of Cities Logo" />
+	<h1>Hello world!</h1>
+	<p>
+		(Simple <a href="https://svelte.dev">Svelte</a> page to build upon from)
+	</p>
+	<Counter/>
+</main>
+
+<style>
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+		Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+	:global(body) {
+		padding: 0px;
+		margin: 0px;
+	}
+
+	main {
+		text-align: center;
+		padding: 0px;
+		margin: 0px;
+	}
+	#bar {
+		height: 50px;
+		background-color: #1E3765;
+		margin-bottom: 20px;
+	}
+
+	img {
+		height: 16rem;
+		width: 16rem;
+	}
+
+	h1 {
+		color: #0D534D;
+		text-transform: uppercase;
+		font-size: 4rem;
+		font-weight: 100;
+		line-height: 1.1;
+		margin: 2rem auto;
+		max-width: 14rem;
+	}
+
+	p {
+		margin: 1rem auto;
+		line-height: 1.35;
+	}
+
+	@media (min-width: 480px) {
+	h1 {
+		max-width: none;
+	}
+
+	p {
+		max-width: none;
+	}
+	}
+</style>
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,34 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button on:click={increment}>
+  Clicks: {count}
+</button>
+
+<style>
+  button {
+    font-family: inherit;
+    font-size: inherit;
+    padding: 1em 2em;
+    color: #DC4633;
+    background-color: #ffffff;
+    border-radius: 2em;
+    border: 2px solid #DC4633;
+    outline: none;
+    width: 200px;
+    font-variant-numeric: tabular-nums;
+    cursor: pointer;
+  }
+
+  button:focus {
+    border: 2px solid #DC4633;
+  }
+
+  button:active {
+    background-color: #ffbcbc;
+  }
+</style>
